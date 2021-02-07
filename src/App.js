b43a3f728bd37d3798bcf25f2cc16f413b003b01
Cody Husek <codyhusek@HuSoft-MacBook-Pro.local>
2021-02-06 19:20:20 -0500

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import Body from "./Body/index";
+import Footer from "./Footer/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page-container">
+      <header className="header">
+        Welcome to <strong>ScoreTrackr</strong>
       </header>
+      <Body />
+      <Footer />
     </div>
   );
 }
